3bc5b7dad27b97f8f71c9b1bd020d582f0d7126c
bpicassokennedy <b3llakennedy@gmail.com>
2025-01-06 08:00:41 +0000

    okay

--- driver.cpp
+++ driver.cpp
@@ -1,3 +1,8 @@
+/* Programmer: Bella Picasso-Kennedy
+   Date Completed: January 5, 2025 
+   Purpose: CS326 HW4 (write a modular program in C++ for rotating a 2D array)
+*/
+
 #include <iostream>
 #include <fstream>
 
@@ -10,16 +15,16 @@ void printArray(int rows, int columns, int arr[][MAX]);
 
 int main(){
     int rows = 0, columns = 0;
-    int originalArr[MAX][MAX]; //max row + column count
+    int originalArr[MAX][MAX]; 
 
-    readFile(rows, columns, INPUT_FILE, originalArr);
+    readFile(rows, columns, INPUT_FILE, originalArr); //reads input file and stores its contents in a 2d array
     
     std::cout << "original 2d array: " << std::endl;
-    printArray(rows, columns, originalArr);
+    printArray(rows, columns, originalArr); //prints given 2d array to the terminal 
 
     int newArr[MAX][MAX];
     std::cout << std::endl << "rotated 2d array: " << std::endl;
-    rotateArray(rows, columns, originalArr, newArr);
+    rotateArray(rows, columns, originalArr, newArr); //rotates any given 2d array 90 degrees clockwise
     printArray(columns, rows, newArr);
 }
 
@@ -47,7 +52,7 @@ void readFile(int& rows, int& columns, std::string fileName, int arr[][MAX]){
 void rotateArray(int rows, int columns, int arr[][MAX], int rotatedArr[][MAX]){
     for(int i = 0; i < rows; i ++){
         for(int j = 0; j < columns; j++){
-            rotatedArr[j][rows - i - 1] = arr[i][j]; //first column becomes the first row, second column becomes the second row, and so on and so forth
+            rotatedArr[j][rows - i - 1] = arr[i][j]; //first column becomes the first row, second column becomes the second row, and so on and so forth!
         }
     }
 }
